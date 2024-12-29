c9a6fe18fdc7e94618cf37d00e380bdcab7ea81c
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-12-29 21:50:52 +0000

    routes/jukebox/+page.svelte - move content into JukeboxBlock.svelte

---
+++ src/lib/JukeboxBlock.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { baseUrl } from '$lib/app.ts';
+  import SongBlock from "$lib/SongBlock.svelte";
+  import { page } from "$app/stores";
+  let { jukebox } = $page.data;
+</script>
+
+<h1>Playlist: {jukebox.playing? "playing": "stopped"}
+  index {jukebox.currentIndex}
+  at position {jukebox.position} gain {jukebox.gain}</h1>
+<div>
+  <div class="navigtion">
+    <button on:click={() => jukebox.start()} >
+      start
+    </button>
+    <button on:click={() => jukebox.stop()} >
+      stop
+    </button>
+    <button on:click={() => jukebox.skipSong()} >
+      skip song
+    </button>
+    <button on:click={() => jukebox.clear()} >
+      clear playlist
+    </button>
+    <button on:click={() => jukebox.shuffle()} >
+      shuffle playlist
+    </button>
+    <button on:click={() => jukebox.add("7a97f7af162a1962d252b195f737cd9f")} >
+      add
+    </button>
+  </div>
+  <ul>
+    <li>
+      {#each jukebox.entry as entry, index}
+        <div class="controls">
+          <button on:click={() => jukebox.remove(index)} >
+            remove
+          </button>
+        </div>
+        <SongBlock song={entry} />
+      {/each}
+    </li>
+  </ul>
+  <div>
+  </div>
+</div>
