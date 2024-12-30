5275319d5947f9de3cf17363177059222c7debf6
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-12-30 23:11:17 +0000

    vite.config.ts - include Icons plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), Icons({ compiler: 'svelte' })],
 
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
