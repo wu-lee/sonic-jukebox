ddae2a9b05ceea22c9557576673363e65d725e96
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-01 18:23:34 +0000

    lib/SongColumns.ts - drop the unnecessary columns
    Since we now have a way to access the original row value

--- src/lib/SongColumns.ts
+++ src/lib/SongColumns.ts
@@ -1,10 +1,14 @@
-import type { CellContext, ColumnDef, Row } from '@tanstack/table-core';
+import {
+  createColumnHelper,
+  type CellContext,
+  type ColumnDef,
+  type Row
+} from '@tanstack/table-core';
 import { createRawSnippet } from 'svelte';
 import { renderComponent, renderSnippet } from '$lib/components/ui/data-table/index.js';
 import { baseUrl } from '$lib/app.js';
 import { page } from '$app/state';
 import type { SubsonicSession } from './subsonic.svelte';
-import { getValues } from '$lib/components/ui/data-table/data-table.svelte';
 import ButtonCell from '$lib/ButtonCell.svelte';
 import type { Child } from 'subsonic-api';
 
@@ -15,51 +19,41 @@ const artistUrl = (id: string) => `${baseUrl}/artist/${id}`;
 const albumUrl = (id: string) => `${baseUrl}/album/${id}`;
 const songUrl = (id: string) => `${baseUrl}/song/${id}`;
 
-function mkLinkCellSnippet(
+function mkLinkCellRenderer<T>(
   altLabel: string,
-  idParam: string,
-  nameParam: string,
+  idParam: keyof T,
+  nameParam: keyof T,
   toUrl: (id: string) => string
 ) {
-  return createRawSnippet<[Record<string, unknown>]>((getParams) => {
+  const snippet = createRawSnippet<[T]>((getParams) => {
     const params = getParams();
     const id = params[idParam];
     const name = params[nameParam];
-    const url = toUrl(id as string);
+    const url = toUrl(String(id));
     return {
       render: () => `<div><a href="${url}" alt="${altLabel}">${name}</a></div>`
     };
   });
-}
-
-function mkLinkCellRenderer<T>(
-  altLabel: string,
-  idParam: string,
-  nameParam: string,
-  toUrl: (id: string) => string
-) {
-  const snippet = mkLinkCellSnippet(altLabel, idParam, nameParam, toUrl);
-  return ({ row }: { row: Row<T> }) => renderSnippet(snippet, getValues(row, [idParam, nameParam]));
+  return ({ row }: { row: Row<T> }) => renderSnippet(snippet, row.original);
 }
 
 function mkButtonCellRenderer<T, U>(
   label: string,
-  idParam: string,
+  idParam: keyof T,
   clickHandler: (cc: CellContext<T, U>) => void
 ) {
   return (context: CellContext<T, U>) => {
     const row = context.row;
-    const values = getValues(row, [idParam]);
     const onclick = () => {
       clickHandler(context);
     };
-    return renderComponent(ButtonCell, { label, songId: values[idParam], onclick });
+    return renderComponent(ButtonCell, { label, songId: row.original[idParam], onclick });
   };
 }
 
+const ch = createColumnHelper<Child>();
+
 export const commonSongColumns: ColumnDef<Child>[] = [
-  { accessorKey: 'artistId', header: 'Artist Id' },
-  { accessorKey: 'albumId', header: 'Album Id' },
   {
     accessorKey: 'artist',
     header: 'Artist',
@@ -76,7 +70,14 @@ export const commonSongColumns: ColumnDef<Child>[] = [
     cell: mkLinkCellRenderer('song info', 'id', 'title', songUrl)
   },
   { accessorKey: 'year', header: 'Year' },
-  { accessorKey: 'created', header: 'Created' },
+  {
+    accessorKey: 'created',
+    header: 'Created',
+    cell: (props) => {
+      const created = props.row.original.created;
+      return created === undefined ? '-' : new Date(created).toLocaleString();
+    }
+  },
   { accessorKey: 'duration', header: 'Duration' }
 ];
 
