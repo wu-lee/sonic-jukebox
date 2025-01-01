499e02ede03cc3f9d41a24fcc7cb33e79d58865e
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-01 17:03:12 +0000

    lib/ButtonCell.svelte - use the Button UI component

--- src/lib/ButtonCell.svelte
+++ src/lib/ButtonCell.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import Button from "$lib/components/ui/button/button.svelte"
   let { label, songId, onclick } = $props();
   let { jukebox } = page.data;
 </script>
 
 <div class="navigation">
-  <button onclick={onclick} >
+  <Button onclick={onclick} >
     {label}
-  </button>
+  </Button>
 </div>
