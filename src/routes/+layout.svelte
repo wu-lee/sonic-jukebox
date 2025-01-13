40aa3f7694d1c6ec22f36e568402c0a3b3969a7a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-13 23:19:33 +0000

    routes/+layout.svelte - put directory and jukebox into scrollboxes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 
 <script lang="ts">
   import * as Tabs from "$lib/components/ui/tabs/index.js";
+  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
   import '../app.css';
   import JukeboxBlock from '$lib/JukeboxBlock.svelte';
   import { baseUrl } from '$lib/app.ts';
@@ -16,10 +17,12 @@
       <Tabs.Trigger value="jukebox">Jukebox</Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content value="directory">
-      <nav><a href={`${baseUrl}/`}>Index</a></nav>
-      <div class="children">
-        {@render children()}
-      </div>
+      <ScrollArea class="h-[100vh] w-[100vw]">
+        <nav><a href={`${baseUrl}/`}>Index</a></nav>
+        <div class="children">
+          {@render children()}
+        </div>
+      </ScrollArea>
     </Tabs.Content>
     <Tabs.Content value="jukebox">
       <JukeboxBlock />
