7652099a042d69a4174cb0eacb7d549a3f00219e
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-13 23:14:09 +0000

    routes/+layout.svelte - put jukebox into a tab
    And directory into the main tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts" module>
 </script>
 
-<script>
+<script lang="ts">
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
   import '../app.css';
   import JukeboxBlock from '$lib/JukeboxBlock.svelte';
   import { baseUrl } from '$lib/app.ts';
@@ -9,9 +10,19 @@
 </script>
 
 <div class="top">
-  <nav><a href={`${baseUrl}/`}>Index</a></nav>
-  <div class="children">
-    {@render children()}
-  </div>
-  <JukeboxBlock />
+  <Tabs.Root value="directory" class="w-[100%]">
+    <Tabs.List class="w-[100%] justify-stretch" >
+      <Tabs.Trigger value="directory">Directory</Tabs.Trigger>
+      <Tabs.Trigger value="jukebox">Jukebox</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="directory">
+      <nav><a href={`${baseUrl}/`}>Index</a></nav>
+      <div class="children">
+        {@render children()}
+      </div>
+    </Tabs.Content>
+    <Tabs.Content value="jukebox">
+      <JukeboxBlock />
+    </Tabs.Content>
+  </Tabs.Root>
 </div>
