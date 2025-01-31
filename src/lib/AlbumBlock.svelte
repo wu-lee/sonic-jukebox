80ea896e0e30115c7b45b872d5b3179d352055ba
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-01-31 18:13:29 +0000

    lib/AlbumBlock.svelte - add an "add album" button
    It's a pain to add the tracks one by one

--- src/lib/AlbumBlock.svelte
+++ src/lib/AlbumBlock.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
   import { type AlbumID3 } from "subsonic-api";
+  import ButtonCell from '$lib/ButtonCell.svelte';
   import { baseUrl } from "$lib/app.ts"
+  import { page } from '$app/state';
+  const jukebox = page.data.jukebox;
   interface Props {
     album: AlbumID3;
     coverArtUrl: string;
   }
   let { album, coverArtUrl }: Props = $props();
+  function addAlbum() {
+  console.log("adding ", jukebox, album.id);
+    jukebox.add(album.song.map(song => song.id));
+  }
 </script>
 
 <div>
-  <h1>Album: <a href={`${baseUrl}/album/${album.id}`}>{album.name ?? 'Unknown'}</a></h1>
+  <h1>Album: <a href={`${baseUrl}/album/${album.id}`}>{album.name ?? 'Unknown'}</a> </h1>
   <div>
     <img
       src={coverArtUrl}
       alt="album cover art"
     />
+    <ButtonCell label="add album" onclick={addAlbum}/>
     <!-- img src={album.artistImageUrl ?? ''} /-->
     <!-- FIXME not working
     <div>
